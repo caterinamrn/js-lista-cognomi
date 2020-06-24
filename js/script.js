5808700a2aff42a3b78124d0952c340bf30a91e7
caterina <catemarian1806@gmail.com>
2020-06-24 15:20:19 +0200

    posizione

--- js/script.js
+++ js/script.js
@@ -5,3 +5,9 @@ var cognomeUtente = prompt("Inserire il proprio cognome");
 // versione con sort
   lista.sort();
   console.log(lista);
+
+for (var i = 0; i < lista.length; i++) {
+  if (cognomeUtente == lista[i]) {
+    console.log("la posizione del cognome dell'utente è: ",i+1);
+  }
+}
