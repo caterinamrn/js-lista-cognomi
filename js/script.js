881902f2ce328b6928ba6a7781e8b13d8e7887aa
caterina <catemarian1806@gmail.com>
2020-06-24 15:14:51 +0200

    inizio e ordinamento

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var lista = ["Alberti","Zanier", "Treccani", "Biancato", "Valsi", "Manin"];
+var cognomeUtente = prompt("Inserire il proprio cognome");
+
+  lista.push(cognomeUtente);
+// versione con sort
+  lista.sort();
+  console.log(lista);
